1e4335561faef86ab3ccbfbbe20aad1a2bc818ba
jiajun <wangjiajun925@gmail.com>
2017-03-05 01:41:34 -0500

    End of day upload: changed name of project in Week #1 Fixed mistake in creating graphs for assignment #2

--- Week2to3_DijkstraAlgorithm/Week2to3_DijkstraAlgorithm/Source.cpp
+++ Week2to3_DijkstraAlgorithm/Week2to3_DijkstraAlgorithm/Source.cpp
@@ -1,28 +1,29 @@
 #include<iostream>
+#include<iomanip>
 #include<time.h> //for time()
 using namespace std;
 
+//create a class called route, which has a boolean stating whether this route exists, and an double variable showing the distance if the route exists
+class route {
+public:
+	double get_dist() { return distance; }
+	bool ifconnect() { return is_connect; }
+	void set_connect(bool x) { is_connect = x; }
+	void set_distance(double x) { distance = x; }
+
+	route(bool x = 0, double y = 999) :is_connect(x), distance(y) {}; //constructor
+
+																	  // ?? Should I delte "is_connect","distance"? If yes, how to do that?
+	~route() { //destructor, use to delete items created on the heap
+		cout << "destructor called" << endl;  //but when are they called in the program?
+	}
+
+private:
+	bool is_connect;
+	double distance;
+};
+
 int main() {
-	//create a class called route, which has a boolean stating whether this route exists, and an double variable showing the distance if the route exists
-	class route {
-	public:
-		double get_dist() { return distance; }
-		bool ifconnect() { return is_connect; }
-		void set_connect(bool x) { is_connect = x; }
-		void set_distance(double x) { distance = x; }
-
-		route(bool x = 0, double y = 1.0) :is_connect(x), distance(y) {}; //constructor
-
-		// ?? Should I delte "is_connect","distance"? If yes, how to do that?
-		~route() { //destructor, use to delete items created on the heap
-			cout << "destructor called" << endl;  //but when are they called in the program?
-		}
-
-	private:
-		bool is_connect;
-		double distance;
-	};
-
 	//create a random map of size x, undirectional.
 	int size = 5;
 	double density = 0.2;
@@ -37,34 +38,15 @@ int main() {
 		for (int j = i; j < size; ++j) {
 			if (i == j)
 				graph[i][j].set_connect(false); //this is redundant..but well, I'll keep it here for redundancy
-			else {
-				graph[i][j] = graph[j][i] = (rand() % 100 < 100 * density); //rand() generates a random integer number 
+			else if (rand() % 100 < 100 * density){ //rand() generates a random integer number
+				double dist;
+				dist = rand() % 10;
+				graph[i][j].set_connect(true);
+				graph[i][j].set_distance(dist);
+				graph[j][i].set_connect(true); 
+				graph[j][i].set_connect(dist);
 			}
-			cout << graph[i][j].ifconnect() << endl; // << need to be overloaded for route
 		}
-
-	//Output up until here:
-	/*
-	1
-	destructor called
-	0
-	0
-	destructor called
-	0
-	destructor called
-	0
-	destructor called
-	0
-	0
-	destructor called
-	1
-	destructor called
-	0
-	0
-	destructor called
-	0
-	0
-	*/
 
 	//print out of the map
 	for (int i = 0; i < size; ++i) {
@@ -73,7 +55,33 @@ int main() {
 		cout << endl;
 	}
 
+	//print out distance
+	for (int i = 0; i < size; ++i) {
+		for (int j = 0; j < size; ++j)
+			cout << setw(4) << graph[i][j].get_dist() << ' ';
+		cout << endl;
+	}
+
 	//find the shortest distance
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
